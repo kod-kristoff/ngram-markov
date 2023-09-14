49bcc55b009f6753f37da91e353722d9bd48b530
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-09-14 15:45:53 +0000

    tokenize

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::iter;
 
 use rand::seq::SliceRandom;
 
@@ -18,7 +19,7 @@ impl Brain {
     pub fn train(&mut self, text: &str) {
         let mut prev_word = None;
 
-        for word in text.split(' ') {
+        for word in Self::tokenize(text) {
             if let Some(prev_word) = prev_word.replace(word) {
                 *self
                     .words
@@ -31,7 +32,9 @@ impl Brain {
     }
 
     pub fn prompt(&self, prompt: &str, length: usize) -> String {
-        let mut out: Vec<_> = prompt.split(' ').map(|word| word.to_string()).collect();
+        let mut out: Vec<_> = Self::tokenize(prompt)
+            .map(|word| word.to_string())
+            .collect();
 
         let mut rng = rand::thread_rng();
 
@@ -50,6 +53,62 @@ impl Brain {
             }
         }
 
-        out.join(" ")
+        out.join("")
+    }
+
+    pub fn tokenize(s: &str) -> impl Iterator<Item = &str> {
+        let mut chars = s.char_indices().peekable();
+
+        iter::from_fn(move || loop {
+            let (idx, ch) = chars.next()?;
+
+            if ch.is_alphanumeric() {
+                let idx_from = idx;
+                let mut idx_to = idx + ch.len_utf8();
+
+                while let Some((idx, ch)) = chars.peek() {
+                    if ch.is_ascii_alphanumeric() {
+                        idx_to = idx + ch.len_utf8();
+                        chars.next();
+                    } else {
+                        break;
+                    }
+                }
+                return Some(&s[idx_from..idx_to]);
+            } else {
+                let idx_from = idx;
+                let idx_to = idx + ch.len_utf8();
+
+                return Some(&s[idx_from..idx_to]);
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use test_case::test_case;
+
+    #[test_case(
+        "Hello, World!",
+        &["Hello",",", " ","World", "!"]
+    )]
+    #[test_case(
+        "#include <coffee.h>",
+        &["#","include"," ","<","coffee",".","h",">"]
+    )]
+    #[test_case(
+        "123 + 234 = 0xCAFEBABE",
+        &["123"," ","+"," ","234"," ","="," ","0xCAFEBABE"]
+    )]
+    fn tokenize(given: &str, expected: &[&str]) {
+        let actual: Vec<_> = Brain::tokenize(given).collect();
+
+        // let expected: Vec<_> = expected.iter()
+        //     .map(|token| token.to_string())
+        //     .collect();
+        assert_eq!(actual.as_slice(), expected);
     }
 }
